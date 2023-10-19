ed62be5fb3019191a22c92e14a52b42aa2a7e3f2
E-mitch3 <ewan-mitchell@outlook.com>
2023-10-19 23:40:38 +0100

    fix data set

--- Ewan/data_processing.R
+++ Ewan/data_processing.R
@@ -1,10 +1,10 @@
+library(dplyr)
+
 flightData <- read.csv("C:/Users/USER/OneDrive/Bristol Year 4/Data Science Toolbox/Group Assessments/full_data_flightdelay.csv")
 
 unique(flightData$DEPARTING_AIRPORT)
 
 jfk_flight_data <- flightData[flightData$DEPARTING_AIRPORT == "John F. Kennedy International",]
-
-jfk_flight_data %>% select("")
 
 cols.dont.want <- c("DEPARTING_AIRPORT","AIRPORT_FLIGHTS_MONTH","LATITUDE", "LONGITUDE", "AVG_MONTHLY_PASS_AIRPORT")
 
@@ -12,4 +12,4 @@ jfk_flight_data <- jfk_flight_data[, ! names(jfk_flight_data) %in% cols.dont.wan
 
 View(jfk_flight_data)
 
-write.csv2(jfk_flight_data, file = "jfk_flight_data.csv")
+write.csv(jfk_flight_data, file = "jfk_flight_data.csv", sep = "\t", row.names=FALSE)
